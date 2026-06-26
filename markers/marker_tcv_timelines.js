0756e29fd3068f0fa05d740b6d75f502b00b2a87
Jaikumar Govindan - j0g0ph1 <Jaikumar.Govindan@walmart.com>
2026-06-26 15:44:01 -0700

    Adding TCV Audio Remove Snap button and its JS func

--- markers/marker_tcv_timelines.js
+++ markers/marker_tcv_timelines.js
@@ -1,6 +1,7 @@
 window.MARKER_DATABASE = window.MARKER_DATABASE || {};
 window.MARKER_DATABASE['TCV.0.steps'] = [
+    { "p": 1, "step1": [[0, 4.4], [4.4, 8.2], [8.2, 12.6], [12.6, 16.8]], "step2": [[0, 8.2], [8.2, 16.8]], "step3": [[0, 16.8]], "step4": [0, 16.8] },
+    { "p": 2, "step1": [[16.8, 20.2], [20.2, 23.7], [23.7, 26.5], [26.5, 30.6]], "step2": [[16.8, 23.7], [23.7, 30.6]], "step3": [[16.8, 30.6]], "step4": [16.8, 30.6] }
 ];
-
 window.MARKER_DATABASE['TCV.1.steps'] = [
 ];
