b91f4c2494cff694c281a6379b18e850e8ff4d0b
mukundan <mukundan.narayanan@gmail.com>
2026-07-23 21:09:52 -0700

    tvt

--- aruLicheyal/TVT/markers/marker_tvt_timelines.js
+++ aruLicheyal/TVT/markers/marker_tvt_timelines.js
@@ -1,1 +1,5 @@
 window.MARKER_DATABASE = window.MARKER_DATABASE || {};
+
+window.MARKER_DATABASE['TVT.0.steps'] = [
+  { "p": 1, "step1": [[0,4.1],[4.1,8],[8,11.9],[11.9,16.4]], "step2": [[0,4.1],[4.1,8],[8,11.9],[11.9,16.4]], "step3": [[0,8],[8,16.4]], "step4": [0,16.4] }
+];
